9f507937d52d5194697ba8f89abdd156c6a4e82a
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-04-05 16:13:22 -0300

    create generic styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/settings/_spaces.css';
+import './styles/generic/_box-sizing.css';
+import './styles/generic/_resets.css';
+import './styles/components/_title.css';
 
 ReactDOM.render(
   <React.StrictMode>
