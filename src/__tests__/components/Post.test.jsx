4e2dd0b300c06388e4778b6762dc99f49d2e7d6b
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-04-05 16:13:52 -0300

    create Post component

---
+++ src/__tests__/components/Post.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import Post from '../../components/Post';
+
+describe('<Post />', () => {
+  describe('When the component is rendered', () => {
+    const post = {
+      title: 'post title',
+      body: 'post body'
+    }
+
+    it('should show the post title passed in props', () => {
+      render(<Post post={post} />)
+
+      const postTitleElement = screen.getByRole('heading', { name: /post title/i });
+      
+      expect(postTitleElement).toBeInTheDocument();
+    });
+
+    it('should show the post body passed in props', () => {
+      render(<Post post={post} />)
+
+      const postBodyElement = screen.getByText(/post body/i);
+      
+      expect(postBodyElement).toBeInTheDocument();
+    });
+  });
+});
